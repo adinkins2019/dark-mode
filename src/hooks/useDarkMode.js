140a827c341c98846edb8292d3160dc30a6bee31
adinkins2019 <afdinki@gmail.com>
2019-10-08 21:23:46 -0400

    Fixed errors in Navbar useDarkMode and useLocalStorage

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,12 +1,12 @@
-import React, {useState, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import useLocalStorage from './useLocalStorage'
 
 const useDarkMode = ({key}) => {
     const [darkMode, setDarkMode] = useLocalStorage(key)
     let bodyTag = document.querySelector('body');
-    useEffect(()=
+    useEffect(()=>
         bodyTag.classList.toggle('dark-mode', darkMode)
-    , [])
+    , [darkMode, bodyTag.classList])
     return [darkMode, setDarkMode]
 };
 
--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -5,12 +5,13 @@ const useLocalStorage = (key, initialValue) => {
         const item = window.localStorage.getItem(key)
         return item? JSON.parse(item): initialValue;
     })
-    return [storedValue];
+    
 
     const setValue = value => {
         setStoredValue(value);
-        windows.localStorage.setItem(key, JSON.stringify(value));
+        window.localStorage.setItem(key, JSON.stringify(value));
     }
+    return [storedValue, setValue];
 };
 
 export default useLocalStorage;
